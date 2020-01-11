67c652beba7b337bbd83ca39dca78fb7b346bb1a
dlrzz <arkad.gawry@gmail.com>
2020-01-11 17:07:24 +0100

    Added external link to information

--- app/src/main/java/com/example/casinoandroid/fragment/InformationFragment.java
+++ app/src/main/java/com/example/casinoandroid/fragment/InformationFragment.java
@@ -1,6 +1,8 @@
 package com.example.casinoandroid.fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.casinoandroid.R;
 
@@ -27,7 +30,15 @@ public class InformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_information, container, false);
+        View view =  inflater.inflate(R.layout.fragment_information, container, false);
+        view.findViewById(R.id.btnLearnMore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://boardgamegeek.com/boardgame/149896/alien-vs-predator-hunt-begins"));
+                startActivity(launchBrowser);
+            }
+        });
+        return view;
     }
 
 }
