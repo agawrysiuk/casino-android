bd7c25d58edb5bfb60b6e2884e55dfbf6b57c690
dlrzz <arkad.gawry@gmail.com>
2020-01-11 16:18:46 +0100

    Updated Tab implementation

--- app/src/main/java/com/example/casinoandroid/MainActivity.java
+++ app/src/main/java/com/example/casinoandroid/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.casinoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.example.casinoandroid.fragment.TabAdapter;
+import com.google.android.material.tabs.TabLayout;
 import com.parse.ParseInstallation;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabAdapter tabAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        viewPager = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(tabAdapter);
+        tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager,true);
+
     }
 }
